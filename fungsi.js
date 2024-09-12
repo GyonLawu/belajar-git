6124beee6ac842a97419849acbb353c1d1d6d4fe
Gyon Lawu <gyonlawu@gmail.com>
2024-09-13 02:54:11 +0700

    Menambahkan fungsi bagi

--- fungsi.js
+++ fungsi.js
@@ -2,7 +2,10 @@ const add = (a, b) => a + b ;
 console.log(add(1, 2));
 
 const subtract = (a, b) => a - b;
-console.log(subtract(5,2));
+console.log(subtract(5, 2));
 
 const multiply = (a, b) => a * b;
 console.log(multiply(2, 2));
+
+const divide = (a, b) => a / b;
+console.log(divide(4, 2));
