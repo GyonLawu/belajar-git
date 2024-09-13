13ef90fe8c09478cee6b5917e6f00b79caf83981
Gyon Lawu <gyonlawu@gmail.com>
2024-09-14 02:51:39 +0700

    Menambahkan fungsi math sin

--- fungsi.js
+++ fungsi.js
@@ -7,4 +7,5 @@ console.log(subtract(5,2));
 const multiplyNumber = (a, b) => a * b;
 console.log(multiplyNumber(2, 2));
 
-console.log(add(5, 5));
+const mathsin = (number) => Math.sin(number);
+console.log(mathsin(10));
