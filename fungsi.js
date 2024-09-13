551b06e8d72651c7157c7570b4655f6b45d7ae09
Gyon Lawu <gyonlawu@gmail.com>
2024-09-13 21:50:45 +0700

    Merevisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -2,10 +2,7 @@ const add = (a, b) => a + b ;
 console.log(add(1, 2));
 
 const subtract = (a, b) => a - b;
-console.log(subtract(5, 2));
+console.log(subtract(5,2));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(2, 2));
-
-const divide = (a, b) => a / b;
-console.log(divide(4, 2));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(2, 2));
